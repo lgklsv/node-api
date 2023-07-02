a43706c369047843dfa675c399e374101a894a82
lgklsv <kolosov20011@gmail.com>
2023-07-02 08:22:28 +0300

    init: basic project setup

---
+++ src/server.ts
@@ -0,0 +1,21 @@
+import http from 'http';
+import 'dotenv/config';
+
+const PORT = process.env.PORT || 5137;
+
+const server = http.createServer(async (req, res) => {
+  if (req.url === '/api' && req.method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    res.write('Hi there, This is a Vanilla Node.js API');
+
+    res.end();
+  } else {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route not found' }));
+  }
+});
+
+server.listen(PORT, () => {
+  console.log(`server started on port: ${PORT}`);
+});
